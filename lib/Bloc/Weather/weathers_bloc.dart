02740204ff8b367ebd9f7d5d0a4a7958be4b3204
sifat hassan <sifat007.sh7@gmail.com>
2022-10-21 12:33:31 +0600

    both approch are writen

--- lib/Bloc/Weather/weathers_bloc.dart
+++ lib/Bloc/Weather/weathers_bloc.dart
@@ -14,34 +14,59 @@ class WeathersBloc extends Bloc<WeathersEvent, WeathersState> {
    WeatherRepository? weatherRepository=WeatherRepository();
   WeathersBloc() : super(WeathersInitial()) {
 
+    ///if go with this approch
+    ///Stream<WeathersState> mapEventToState(WeathersEvent event) async*{   ///hide this functions
+    // on<FetchWeather>((event, emit)async{
+    //   emit(WeatherLoading()) ;
+    //   try {
+    //     final Weather weather = await weatherRepository!.getWeather(event.city!);
+    //     emit( WeatherLoaded(weather: weather));
+    //   } catch (error) {
+    //     print(error);
+    //     emit( WeatherError());
+    //   }
+    // });
+    //
+    // on<RefreshWeather>((event, emit)async{
+    //   emit(WeatherLoading()) ;
+    //   try {
+    //     final Weather weather = await weatherRepository!.getWeather(event.city!);
+    //     emit (WeatherLoaded(weather: weather));
+    //   } catch (error) {
+    //     print("Error" + error.toString());
+    //     emit (state);
+    //   }
+    // });
+    //
+    // on<ResetWeather>((event, emit)async{
+    //   emit(WeatherLoading()) ;
+    //   emit(WeatherEmpty());
+    // });
 
-    on<FetchWeather>((event, emit)async{
-      emit(WeatherLoading()) ;
+  }
+
+  @override
+  Stream<WeathersState> mapEventToState(WeathersEvent event) async*{
+    // TODO: implement mapEventToState
+    if (event is FetchWeather) {
+      yield WeatherLoading();
       try {
         final Weather weather = await weatherRepository!.getWeather(event.city!);
-        emit( WeatherLoaded(weather: weather));
+        yield WeatherLoaded(weather: weather);
       } catch (error) {
         print(error);
-        emit( WeatherError());
+        yield WeatherError();
       }
-    });
-
-    on<RefreshWeather>((event, emit)async{
-      emit(WeatherLoading()) ;
+    } else if (event is RefreshWeather) {
       try {
         final Weather weather = await weatherRepository!.getWeather(event.city!);
-        emit (WeatherLoaded(weather: weather));
+        yield WeatherLoaded(weather: weather);
       } catch (error) {
         print("Error" + error.toString());
-        emit (state);
+        yield state;
       }
-    });
-
-    on<ResetWeather>((event, emit)async{
-      emit(WeatherLoading()) ;
-     emit(WeatherEmpty());
-    });
-
-
+    } else if (event is ResetWeather) {
+      yield WeatherEmpty();
+    }
   }
 }
